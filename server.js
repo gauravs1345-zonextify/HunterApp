f25cc20e3f86beef6e59bc80feb29668d39f4fca
Gaurav Singh <gaura@MacBook-Air.local>
2025-08-30 23:22:17 +0530

    New app created

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import listingRoutes from "./routes/listingRoutes.js";
+import syncRoutes from './routes/syncData.js';
+// import { sendToZoho } from './crmData.js';
+// import { sendToZoho } from './crmData.js';
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+// Get All MongoDB Data
+app.use("/listings", listingRoutes);
+app.use("/sync", syncRoutes);
+
+
+app.post('/sync', async (req, res) => {
+  const contact = req.body;
+  await sendToZoho(contact);
+  res.send('Contact sent to Zoho CRM');
+});
+
+
+// Show HTMl Page data
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+ 
